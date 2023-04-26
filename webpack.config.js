55216e7a2d73d4ac0b5741423df6b1a9768bfce5
abiola <abiola.arobieke@gmail.com>
2023-04-26 14:55:33 -0400

    Modify webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -43,17 +43,6 @@ module.exports = {
                 type: 'srcset',
               },
             ],
-            urlFilter: (attribute, value, resourcePath) => {
-              // The `attribute` argument contains a name of the HTML attribute.
-              // The `value` argument contains a value of the HTML attribute.
-              // The `resourcePath` argument contains a path to the loaded HTML file.
-
-              if (/example\.pdf$/.test(value)) {
-                return false;
-              }
-
-              return true;
-            },
           },
         },
       },
